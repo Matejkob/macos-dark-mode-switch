c41ac18aab9ae77184cbcdcce6f6aafd901c6d9b
Mateusz Bąk <bakmatthew@icloud.com>
2025-06-20 13:06:34 +0200

    Initial Commit

---
+++ DarkModeSwitch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DarkModeSwitch
+//
+//  Created by Mateusz Bąk on 6/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DarkModeSwitch/DarkModeSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarkModeSwitchApp.swift
+//  DarkModeSwitch
+//
+//  Created by Mateusz Bąk on 6/20/25.
+//
+
+import SwiftUI
+
+@main
+struct DarkModeSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
